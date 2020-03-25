c310cba94866a1338c82979a57814e2864f19c25
Deney Araujo <deneyaraujo@gmail.com>
2020-03-25 12:34:57 -0300

    Add template marielle franco

--- wordCloud.py
+++ wordCloud.py
@@ -19,7 +19,7 @@ dirWork = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
 text = open(path.join(dirWork, 'boto_rosa.txt')).read()
 
 #Dados de entrada e saida
-maskIn = "templates/twitter_mask.jpg"
+maskIn = "templates/marielle_mask.png"
 output = "wordImages/"+"word_"+maskIn.rsplit("/")[1]
 
 # read the mask image
